eae43ee01d75dac8a6d50f7cff7e7aa184a5f149
Roberto Robles <robertorobles0@gmail.com>
2015-09-17 16:24:50 -0700

    isBPCuffAvailable should use getThreadPool context

--- src/android/co/vitallabs/plugin/Ihealth.java
+++ src/android/co/vitallabs/plugin/Ihealth.java
@@ -97,7 +97,7 @@ public class Ihealth extends CordovaPlugin {
     private void isBP5CuffAvailable(CallbackContext callbackContext) {
       final CordovaPlugin plugin = (CordovaPlugin) this;
       
-      cordova.getActivity().runOnUiThread(new Runnable() {
+      cordova.getThreadPool().execute(new Runnable() {
           @Override
           public void run () {
             Log.i(TAG, "Before running the thread");
