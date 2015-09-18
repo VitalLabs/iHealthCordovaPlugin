17552313f1da9de26cc22096309dbd8ad513a994
Roberto Robles <robertorobles0@gmail.com>
2015-09-18 14:05:17 -0700

    Add more logging to Activity lifecycle functions so we know where is the leak

--- src/android/co/vitallabs/plugin/IhealthActivity.java
+++ src/android/co/vitallabs/plugin/IhealthActivity.java
@@ -119,7 +119,32 @@ public class IhealthActivity extends Activity implements
     }
   }
   // EOF OnActivityResult
+
+  @Override
+  protected void onStart() {
+    Log.i(TAG, "onStart");
+    super.onStart();
+  }
+
+  @Override
+  protected void onRestart() {
+    Log.i(TAG, "onRestart");
+    super.onRestart();
+  }
+
+  @Override
+  protected void onResume() {
+    Log.i(TAG, "onResume");
+    super.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    Log.i(TAG, "onPause");
+    super.onPause();
+  }
   
+  @Override
   protected void onStop() {
     Log.i(TAG, "onStopActivity");
     super.onStop();
