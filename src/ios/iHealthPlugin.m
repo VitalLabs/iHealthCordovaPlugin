7b59928ff41ad02a36a1b5cb1d6995ced5d1825c
Roberto Robles <robertorobles0@gmail.com>
2016-01-14 16:10:54 -0800

    Fix error in evalJs call

--- src/ios/iHealthPlugin.m
+++ src/ios/iHealthPlugin.m
@@ -172,7 +172,7 @@ NSString *available = nil;
   NSArray *bp7DeviceArray = [bp7Controller getAllCurrentBP7Instace];
   NSArray *bp5DeviceArray = [bp5Controller getAllCurrentBP5Instace];
 
-  [self.commandDelegate @"console.log('HELLO CONSOLE.LOG FROM NATIVE SIDE')"];
+  [self.commandDelegate evalJs:@"console.log('HELLO CONSOLE.LOG FROM NATIVE SIDE')"];
   
   if(bp5DeviceArray.count) {
     available = @"BP5";
